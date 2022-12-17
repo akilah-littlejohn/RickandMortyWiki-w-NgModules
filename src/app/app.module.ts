9722dddfc07fe6b03befbab041700ec18516cfe9
akilah-littlejohn <akilahlittlejohn@gmail.com>
2022-12-17 17:56:58 -0600

    2 files modified, 6 added, 1 deleted

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
-import { HelloComponent } from './hello.component';
+import { CharCardComponent } from './char-card/char-card.component';
+import { SearchbarComponent } from './searchbar/searchbar.component';
+import { HomecomponentComponent } from './homecomponent/homecomponent.component';
 
 @NgModule({
-  imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, HelloComponent ],
-  bootstrap:    [ AppComponent ]
+  imports: [BrowserModule, FormsModule],
+  declarations: [
+    AppComponent,
+    CharCardComponent,
+    SearchbarComponent,
+    HomecomponentComponent,
+  ],
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
---
+++ src/app/char-card/char-card.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-char-card',
+  templateUrl: './char-card.component.html',
+  styleUrls: ['./char-card.component.css']
+})
+export class CharCardComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
