b37ffeaf3d3854f6436fe7fe9acea90e5018fda0
akilah-littlejohn <akilahlittlejohn@gmail.com>
2022-12-19 10:05:38 -0600

    1 file modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,16 @@
 import { Component, OnInit, VERSION } from '@angular/core';
 import { CharacterdataService } from './services/characterdata.service';
+import { Input } from '@angular/core';
+import { Character } from './model/character';
 
 @Component({
   selector: 'my-app',
   templateUrl: './app.component.html',
-  styleUrls: [ './app.component.css' ]
+  styleUrls: ['./app.component.css'],
 })
-export class AppComponent implements OnInit  {
-  constructor(public api:CharacterdataService){
-  }
-  ngOnInit(){
+export class AppComponent implements OnInit {
+  @Input() character!: Character;
 
-  }
+  constructor(public api: CharacterdataService) {}
+  ngOnInit() {}
 }
